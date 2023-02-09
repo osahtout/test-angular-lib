e05b685595db800713269b5a7f8c33718aed8e2f
Omar Sahtout <omar.sahtout@sap.com>
2023-02-09 15:20:14 -0500

    init

---
+++ projects/lib/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/lib.service';
+export * from './lib/lib.component';
+export * from './lib/lib.module';
---
+++ projects/lib/src/lib/lib.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'library-test',
+  templateUrl: './lib.component.html',
+  styles: [],
+})
+export class LibComponent {}

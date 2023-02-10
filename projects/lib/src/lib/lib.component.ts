598a121bab4f81627aadef99df84e5bf7740724d
Omar Sahtout <omar.sahtout@sap.com>
2023-02-10 11:17:10 -0500

    test 2 way

--- projects/lib/src/lib/lib.component.ts
+++ projects/lib/src/lib/lib.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'library-test',
   templateUrl: './lib.component.html',
   styles: [],
 })
-export class LibComponent {}
+export class LibComponent{
+  @Input() data?: string;
+
+  @Output() charactersInString = new EventEmitter<number>();
+
+  ngOnInit() {
+    this.charactersInString.emit(this.data!.length);
+  }
+
+  sendData() {
+    this.charactersInString.emit(this.data!.length);
+  }
+
+
+}
